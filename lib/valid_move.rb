fc2b893dd2007729cdc4d66b8471a99d6eed844c
Pamela Bortnick <pbortnick@gmail.com>
2018-07-24 23:12:04 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
-def valid_move?(board, index)
-  if index >= 0 && index <= 8 && board[index] ==  " "
-    true
-  else
-    false
-  end
-end
+# def valid_move?(board, index)
+#   if index >= 0 && index <= 8 && board[index] ==  " "
+#     true
+#   else
+#     false
+#   end
+# end
